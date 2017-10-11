63f3317dc856870896aafa1316667391387117af
gzegorzsabiniok <grzesab111@gmail.com>
2017-10-11 22:42:58 +0200

    new map system part 2

---
+++ Assets/Project/Components/01_Map/Scripts/MapGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Block[] listOfBlocks;
+    private Dictionary<string, Block> blocks = new Dictionary<string, Block>();
+    [SerializeField]
+    private MapSetings settings;
+    private Block[,,] map;
+    private void Awake()
+    {
+        for (int i = 0; i < listOfBlocks.Length; i++)
+        {
+            blocks.Add(listOfBlocks[i].name, listOfBlocks[i]);
+        }
+    }
+    public Block[,,] GenerateMap()
+    {
+        map = new Block[settings.MapSize.x, settings.MapSize.y, settings.MapSize.z];
+        for (int x = 0; x < settings.MapSize.x; x++)
+        {
+            for (int z = 0; z < settings.MapSize.z; z++)
+            {
+                GetHeight(x, z);
+            }
+        }
+        return map;
+    }
+    private void GetHeight(int x, int z)
+    {
+        for (int y = 0; y < settings.MapSize.y; y++)
+        {
+            if (y < 10)
+            {
+                SetBlock(x, y, z, "stone");
+            }
+            else
+            {
+                SetBlock(x, y, z, "air");
+            }
+        }
+    }
+    private void SetBlock(int x, int y, int z, string name)
+    {
+        map[x, y, z] = blocks[name];
+    }
+
+}
--- Assets/Project/Components/01_Map/Scripts/MapManager.cs
+++ Assets/Project/Components/01_Map/Scripts/MapManager.cs
@@ -6,46 +6,18 @@ public class MapManager : MonoBehaviour
 {
     public static MapManager singleton;
     [SerializeField]
-    private MapSetings settings;
-    [SerializeField]
     private int seed;
     private Block[,,] mapGrid;
     [SerializeField]
-    private Block[] listOfBlocks;
-    private Dictionary<string, Block> blocks = new Dictionary<string, Block>();
+    private MapGenerator generator;
     private Chunk[,,] chunks;
-    private void Awake()
+    private void Start()
     {
-        singleton = this;
-        for (int i = 0; i < listOfBlocks.Length; i++)
-        {
-            blocks.Add(listOfBlocks[i].name, listOfBlocks[i]);
-        }
+        GenerateMapGrid();
     }
-
     public void GenerateMapGrid()
     {
-        mapGrid = new Block[settings.MapSize.x, settings.MapSize.y, settings.MapSize.z];
-        for (int x = 0; x < settings.MapSize.x; x++)
-        {
-            for (int z = 0; z < settings.MapSize.z; z++)
-            {
-                GetHeight(x, z);
-            }
-        }
+        mapGrid = generator.GenerateMap();
     }
-    public void GetHeight(int x, int z)
-    {
-        for (int y = 0; y < settings.MapSize.y; y++)
-        {
-            if (y < 10)
-            {
-                mapGrid[x, y, z] = blocks["stone"];
-            }
-            else
-            {
-                mapGrid[x, y, z] = blocks["air"];
-            }
-        }
-    }
+
 }
